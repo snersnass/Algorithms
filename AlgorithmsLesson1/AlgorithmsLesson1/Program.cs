9f6fac64a94dc220f8ea8d54a86f0e764e389f8c
unknown <Karen VanEck@UKHUWWKVE01.zen.internal>
2015-06-29 15:52:23 +0100

    Assignment1 firt commit

---
+++ AlgorithmsLesson1/AlgorithmsLesson1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmsLesson1.UnionFindNs;
+
+namespace AlgorithmsLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = getIntFromConsole("How many points does your set have?");
+     
+      if (n > 0)
+      {
+          int p1 = getIntFromConsole("Enter point p");
+          int p2 = getIntFromConsole("Enter point q");
+          UnionFind uf = new UnionFind(n);
+          while (p1 >= 0 && p2 >= 0)
+          {
+     
+              if (!uf.connected(p1, p2))
+              {
+                  //    Up to here
+                  Console.WriteLine("Connecting {0} to {1}", p1, p2);
+ 
+                  uf.union(p1, p2);
+              }
+              else
+              {
+                  Console.WriteLine("{0} and {1} already connected", p1, p2q);
+              }
+              p1 = getIntFromConsole("Enter point p");
+              p2 = getIntFromConsole("Enter point q");
+          }
+         
+          
+
+      }
+      else
+            {
+                //user gave an illegal input. Handle it here.
+                Console.WriteLine("We can only have a whole number of points - fail!");
+            }
+            Console.WriteLine("...");
+ 
+        }
+
+       static int getIntFromConsole(String prompt)
+        {
+            int n = 0;
+            Console.WriteLine(prompt);
+            if (int.TryParse(Console.ReadLine(), out n))
+            {
+
+            }
+            else
+            {
+                n = -1;
+            }
+            return n;
+        }
+
+    }
+}
---
+++ AlgorithmsLesson1/AlgorithmsLesson1/UnionFindNs/UnionFind.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLesson1.UnionFindNs
+{
+    class UnionFind
+    {
+        private int noElements;
+
+        public UnionFind(int numberOfElements)
+        {
+            noElements = numberOfElements;
+        }
+
+        public void union(int p, int q)
+        {
+
+        }
+
+        public Boolean connected(int p, int q)
+        {
+            return false;
+        }
+
+        public int find(int p)
+        {
+            return -1;
+        }
+
+        public int count()
+        {
+            return -1;
+        }
+    }
+}
